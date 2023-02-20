bcb16fb93eb0304c4f6db71387cb09c620ed1300
chebihiF <chebihi.f@ucd.ac.ma>
2023-02-20 10:45:20 +0100

    add ProductDao.java

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -1,4 +1,33 @@
 package dao;
 
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductDao {
+
+    private List<Product> products = new ArrayList<Product>();
+
+    public Product addProduct(Product product) throws Exception{
+        // add product to DB
+        products.add(product);
+        return product;
+    }
+
+    public Product updateProduct(Product product) throws Exception{
+        // add product to DB
+        Product prod = products
+                .stream()
+                .filter(p -> p.getCode() == product.getCode())
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("product not found"));
+
+
+        return product;
+    }
+
+
+
+
+
 }
