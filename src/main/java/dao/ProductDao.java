a35be5d617ebaa821f05a8c83f6097580bcdbb3b
chebihiF <chebihi.f@ucd.ac.ma>
2023-02-20 11:32:28 +0100

    add ProductMetier.java

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDao {
-
     private List<Product> products = new ArrayList<Product>();
 
     public Product addProduct(Product product) throws Exception{
@@ -21,9 +20,15 @@ public class ProductDao {
                 .filter(p -> p.getCode() == product.getCode())
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("product not found"));
-
+        prod.setLabel(product.getLabel());
+        prod.setPrice(product.getPrice());
+        prod.setQuantity(product.getQuantity());
 
         return product;
+    }
+
+    public List<Product> getProducts() throws Exception{
+        return products;
     }
 
 
--- src/main/java/metier/ProductMetier.java
+++ src/main/java/metier/ProductMetier.java
@@ -1,4 +1,22 @@
 package metier;
 
+import dao.ProductDao;
+import entities.Product;
+
+import java.util.List;
+
 public class ProductMetier {
+
+    private ProductDao dao = new ProductDao();
+
+    public Product addproduct(Product product) throws Exception{
+        if(product.getQuantity()<0 || product.getLabel().trim().equals("") || product.getPrice() <0)
+            throw new RuntimeException("cannot add this product");
+        return dao.addProduct(product);
+    }
+
+    public List<Product> getProducts() throws Exception{
+        return dao.getProducts();
+    }
+
 }
